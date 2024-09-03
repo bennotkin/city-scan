5dc2ca95b86fc4a580a040697e44738fd7ef56dd
Ben Notkin <61945234+bennotkin@users.noreply.github.com>
2024-09-03 09:53:44 -0700

    DRC specific

--- user-inputs.R
+++ user-inputs.R
@@ -1,13 +1,20 @@
 # User inputs for making the R Notebook, so that the calculations sheet only needs code changes
 
 # City
-city <- "Yen Bai"
-country <- "Vietnam"
-map_file_prefices <- c("'Yen_Bai_'", "'Yenbai_'") # Include single quotations around prefices
+city <- "Kinshasa"
+country <- "Democratic Republic of the Congo"
+map_file_prefices <- c("'Kinshasa_'", "'kinshasa_'") # Include single quotations around prefices
 
 # Selecting Benchmark cities (additional cities will be chosen via Oxford Economics and nearby_countries_string)
-bm_cities_manual <- c("Ca Mau", "Cao Lanh", "Can Tho", "Hong Ngu", "Rach Gia", "Hoi An", "Yen Bai", "Cao Bang", "Dien Bien Phu") # NULL when not in use# NULL when not in use; add country as element name (as used by citypopulation.de) when it differs from focus city's country
-nearby_countries_string <- "cambodia|vietnam|thailand|myanmar|burma|malays|brunei"
+bm_cities_manual <- c(
+  "Democratic Republic of Congo" = "Kinshasa",
+  "South Sudan" = "Juba",
+  "Central African Republic" = "Bangui",
+  "Sudan" = "Khartoum",
+  "Somalia" = "Mogadishu",
+  "Nigeria" = "Abuja",
+  "Angola" = "Luanda") # when not in use NULL; add country as element name (as used by citypopulation.de) when it differs from focus city's country
+nearby_countries_string <- "congo|sudan|ugand|rwand|angola|zambia|gabon|cameroo|central|chad|niger"
 
 # If there is no Oxford data, manually set population
 # population_manual <- 
